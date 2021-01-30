1371d1abe5c4a1c92e4e5b00ffea7784c1714a6e
Kevin L. Owens <kowens@gmail.com>
2021-01-30 13:08:34 -0700

    Rename sub-units of currency to Book of Mormon names

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -43,9 +43,9 @@ QString BitcoinUnits::longName(int unit)
     switch(unit)
     {
     case BTC: return QString("DEZ");
-    case mBTC: return QString("lites");
-    case uBTC: return QString("photons");
-    case SAT: return QString("litoshi");
+    case mBTC: return QString("ontis");
+    case uBTC: return QString("senines");
+    case SAT: return QString("leahs");
     default: return QString("???");
     }
 }
@@ -65,9 +65,9 @@ QString BitcoinUnits::description(int unit)
     switch(unit)
     {
     case BTC: return QString("Dezcoins");
-    case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
-    case SAT: return QString("Litoshis (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case mBTC: return QString("Ontis (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Senines (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case SAT: return QString("Leahs (sat) (1 / 100" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
